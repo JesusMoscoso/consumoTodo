0136a1139c5779cfe8bba2a89c86ec4572ab1b40
JesusMoscoso <MoscosoJesus481@gamil.com>
2025-10-02 23:59:52 -0600

    feat:vista de fotos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ListaTodos } from './components/lista-todos/lista-todos';
 import { ListaUsuarios } from './components/lista-usuarios/lista-usuarios';
 import { InfoUsuario } from './components/info-usuario/info-usuario';
 import { InfoTarea } from './components/info-tarea/info-tarea';
+import { Fotos } from './components/fotos/fotos';
 
 export const routes: Routes = [
   { path: 'tareas', component: ListaTodos },
   { path: 'usuarios', component: ListaUsuarios },
   { path: 'usuarios/:id', component: InfoUsuario },
-  {path: 'tareas/:id', component:InfoTarea },
+  { path: 'tareas/:id', component: InfoTarea },
+  { path: 'fotos', component: Fotos },
 ];
